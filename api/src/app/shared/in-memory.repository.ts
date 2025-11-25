48d330891b02a1ee3741da0bbd5583fc6f6ca5fc
Lorenzo Bartolini <lorenzo@greenfutureproject.com>
2025-11-25 17:21:00 +0100

    in-memory skeleton

---
+++ api/src/app/shared/in-memory.repository.ts
@@ -0,0 +1,40 @@
+import { BaseEntity } from "./base-entity";
+import { PaginatedResult, PaginationOptions } from "./pagination.types";
+
+export abstract class InMemoryRepository<T extends BaseEntity, F> {
+
+    protected readonly items: T[] = [];
+
+    createOne(item: T): T {
+        const entity = {
+            ...item,
+            createdAt: new Date(),
+            id: crypto.randomUUID(),
+        }
+        this.items.push(entity);
+        return entity;
+    }
+
+    findMany(filters: F, pagination: PaginationOptions = {}): PaginatedResult<T> {
+        let results = this.items.filter(item => this.matchesFilter(item, filters));
+
+        const { page = 1, limit = results.length } = pagination;
+        const startIndex = (page - 1) * limit;
+        const endIndex = startIndex + limit;
+
+        results = results.slice(startIndex, endIndex);
+
+        const total = results.length;
+        const start = (page - 1) * limit;
+        const data = results.slice(start, start + limit);
+
+        return {
+            data,
+            total,
+            page,
+            limit,
+        };
+    }
+
+    protected abstract matchesFilter(entity: T, filter: F): boolean;
+}
