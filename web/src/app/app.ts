a940ba800b38f91d234359651b997b088b39afee
Lorenzo Bartolini <lorenzo@greenfutureproject.com>
2025-11-27 17:08:47 +0100

    navigate to home when login succesfull

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@viceversa-messaging-app/auth';
 
 @Component({
   imports: [
@@ -9,6 +10,17 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'web';
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.authService.userHasLogged.subscribe((isLogged) => {
+      if (isLogged)
+        this.router.navigate(['/home'])
+    })
+  }
 }
