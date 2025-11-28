3e5fad1229884ee63e80708daad6f08ad42cee39
Lorenzo Bartolini <lorenzo@greenfutureproject.com>
2025-11-28 13:17:38 +0100

    check duplication logic

--- api/src/app/message/message.service.ts
+++ api/src/app/message/message.service.ts
@@ -1,11 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InMemoryRepository } from '../shared/in-memory.repository';
 import { Message } from '../models/message.entity';
 import { MessageFilter } from './message.filter';
+import { PaginationOptions } from '../shared/pagination.types';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MessageService extends InMemoryRepository<Message, MessageFilter> {
-    constructor() {
+
+    logger = new Logger(MessageService.name)
+
+    duplicationTimespanMs = 10000
+
+    constructor(
+        //private config: ConfigService
+    ) {
         super(Message);
+        //this.duplicationTimespanMs = this.config.get('')
+    }
+
+    createOne(item: Message): Message {
+        this.checkDuplicatedMessages(item)
+        return super.createOne(item)
+    }
+
+    private checkDuplicatedMessages(message: Message) {
+        const now = new Date()
+        const createAtLimit = new Date()
+        createAtLimit.setTime(now.getTime() - this.duplicationTimespanMs)
+        const filter = new MessageFilter({ 
+            createdAtFrom: createAtLimit, 
+            createdAtTo: now ,
+            body: message.body,
+            recipientId: message.recipientId,
+            senderId: message.senderId
+        })
+        const foundPossibleDuplicate = this.findMany(filter).data[0]
+        if (foundPossibleDuplicate)
+            throw new Error('Duplicate message')
     }
 }
